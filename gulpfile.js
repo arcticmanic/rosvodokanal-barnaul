f6a1c37fee8d24638a1113500075112403ac26f5
fromaline <55146773+fromaline@users.noreply.github.com>
2020-03-03 14:36:42 +0300

    mid day commit; a lot of changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const del = require('del');
-const browserSync = require('browser-sync').create();
-const sourcemaps = require('gulp-sourcemaps');
-const less = require('gulp-less');
-const smartGrid = require('smart-grid');
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+const del = require('del')
+const browserSync = require('browser-sync').create()
+const sourcemaps = require('gulp-sourcemaps')
+const less = require('gulp-less')
+// const smartGrid = require('smart-grid');
 
-const styleFiles = ['./less/style.less'];
+const styleFiles = ['./less/style.less']
 
-const smartGridOptions = {
-  outputStyle: 'less',
-  maxWidth: '1710px'
-};
+// const smartGridOptions = {
+//   outputStyle: 'less',
+//   maxWidth: '1710px'
+// };
 
 function styles() {
   return gulp
@@ -21,7 +21,7 @@ function styles() {
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(concat('style.css'))
-    .pipe(autoprefixer('> 0.1%'))
+    .pipe(autoprefixer('> 30%'))
     .pipe(
       cleanCSS({
         level: 2
@@ -29,16 +29,16 @@ function styles() {
     )
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 }
 
-function grid(done) {
-  smartGrid('./less/base', smartGridOptions);
-  done();
-}
+// function grid(done) {
+//   smartGrid('./less/base', smartGridOptions);
+//   done();
+// }
 
 function clean() {
-  return del(['./css/styles.css', './css/styles.css.map']);
+  return del(['./css/styles.css', './css/styles.css.map'])
 }
 
 function watch() {
@@ -47,18 +47,18 @@ function watch() {
       baseDir: './',
       index: 'index.html'
     }
-  });
+  })
 
-  gulp.watch('./img').on('change', browserSync.reload);
-  gulp.watch('./js/*.js').on('change', browserSync.reload);
-  gulp.watch('./less/**/*.less', styles);
-  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./img').on('change', browserSync.reload)
+  gulp.watch('./js/*.js').on('change', browserSync.reload)
+  gulp.watch('./less/**/*.less', styles)
+  gulp.watch('./*.html').on('change', browserSync.reload)
 }
 
-gulp.task('grid', grid);
+// gulp.task('grid', grid);
 
-gulp.task('watch', watch);
+gulp.task('watch', watch)
 
-gulp.task('build', gulp.series(clean, gulp.parallel(styles)));
+gulp.task('build', gulp.series(clean, gulp.parallel(styles)))
 
-gulp.task('dev', gulp.series('build', 'watch'));
+gulp.task('dev', gulp.series('build', 'watch'))
