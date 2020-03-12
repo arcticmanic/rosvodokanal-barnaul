e8e7429e5d18415dd95d758907c0ba7fb58dc87c
Fromaline <grechino2412@gmail.com>
2020-03-12 18:07:28 +0300

    huge amount of fixies

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -128,9 +128,9 @@ $(document).ready(function() {
     nextSlide
   ) {
     if (infoSlider.find('.slick-current').hasClass('poor-visibility')) {
-      infoSlider.find('.slick-dots').addClass('slick-dots_visibility')
+      infoSlider.find('.slick-dots').addClass('slick-dots_dark')
     } else {
-      infoSlider.find('.slick-dots').removeClass('slick-dots_visibility')
+      infoSlider.find('.slick-dots').removeClass('slick-dots_dark')
     }
   })
 
@@ -749,45 +749,53 @@ $(document).ready(function() {
         $('.eye-version').on('click', function() {
           $('.eye-version-panel').slideToggle()
           $('.eye-version').toggleClass('eye-version-active')
-          var set = !$container.hasClass('eye-version')
+          var set = !$container.hasClass('eye-version-active')
           if (set) {
             $container
-              .addClass('eye-version')
-              .addClass(
-                'scale-' + $('.eye-version__scale.m--current').data('scale')
-              )
+              .removeClass('eye-version-std')
+              .addClass('eye-version-active')
               .addClass(
                 'color-' + $('.eye-version__color.m--current').data('color')
               )
             $('.header__eye-version_panel').slideDown(300)
-            $('.mob_m').slideUp()
-            $('.btn_mob').removeClass('open')
           } else {
             $container
-              .removeClass('eye-version')
+              .removeClass('eye-version-active')
               .removeClass('scale-1 scale-2 scale-3')
               .removeClass('color-white color-black')
+              .addClass('eye-version-std')
             $('.header__eye-version_panel').slideUp(300)
+            $('.eye-version__scale.m--current').removeClass('m--current')
           }
           eyeVersion.save('set', set)
         })
 
         $('.eye-version__scale').on('click', function(e) {
           e.preventDefault()
-          if ($container.hasClass('eye-version')) {
-            var $this = $(this)
+          var $this = $(this)
+          if (
+            $container.hasClass('eye-version-active') &&
+            !$this.hasClass('m--current')
+          ) {
             $('.eye-version__scale').removeClass('m--current')
             $this.addClass('m--current')
             $container
               .removeClass('scale-1 scale-2 scale-3')
               .addClass('scale-' + $this.data('scale'))
+              .addClass('scale')
             eyeVersion.save('scale', $this.data('scale'))
+          } else if (
+            $container.hasClass('eye-version-active') &&
+            $this.hasClass('m--current')
+          ) {
+            $this.removeClass('m--current')
+            $container.removeClass('scale scale-1 scale-2 scale-3')
           }
         })
 
         $('.eye-version__color').on('click', function(e) {
           e.preventDefault()
-          if ($container.hasClass('eye-version')) {
+          if ($container.hasClass('eye-version-active')) {
             var $this = $(this)
             $('.eye-version__color').removeClass('m--current')
             $this.addClass('m--current')
@@ -801,15 +809,14 @@ $(document).ready(function() {
         $('.eye-version-item_back').on('click', function(e) {
           $('.eye-version-active').removeClass('eye-version-active')
           $('.eye-version').addClass('eye-version-std')
+          $('.eye-version__scale.m--current').removeClass('m--current')
           $('.eye-version-panel').slideToggle()
           e.preventDefault()
           $container
             .removeClass('eye-version')
-            .removeClass('scale-1 scale-2 scale-3')
+            .removeClass('scale scale-1 scale-2 scale-3')
             .removeClass('color-white color-black')
           $('.header__eye-version_panel').slideUp(300)
-          $('.mob_m').slideUp()
-          $('.btn_mob').removeClass('open')
           eyeVersion.save('set', false)
         })
 
