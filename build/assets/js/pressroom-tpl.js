a8f0181c6f5436a0425efd99da9563114beb6470
Fromaline <grechino2412@gmail.com>
2020-03-26 19:12:23 +0300

    some new pages

--- build/assets/js/pressroom-tpl.js
+++ build/assets/js/pressroom-tpl.js
@@ -1,2 +1,9 @@
 "use strict";
+
+$(document).ready(function () {
+  var elWithClammpSizes = {
+    '.std-grid__item .std-grid__title': 3
+  };
+  setClamps(elWithClammpSizes);
+});
 //# sourceMappingURL=pressroom-tpl.js.map
---
+++ js/common/setClamps.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  const elWithClammpSizes = {
+    '.std-grid__item .std-grid__title': 3
+  }
+
+  setClamps(elWithClammpSizes)
+})
