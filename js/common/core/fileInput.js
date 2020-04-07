29fa733f7e7b5ae8bbaf1785f6f6eea091bfebe5
Fromaline <grechino2412@gmail.com>
2020-04-07 12:02:33 +0300

    fix sime bugs

--- js/common/core/fileInput.js
+++ js/common/core/fileInput.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(document)
-    .on('click', '.input-with-label input[type="checkbox"]', function() {
+    .on('click', '.input-with-label input[type="checkbox"]', function () {
       let state = $(this).prop('checked')
       if (state === true) {
         $(this)
@@ -22,36 +22,34 @@ $(document).ready(function() {
           .addClass('required')
       }
     })
-    .on('change', "input[type='file']", function() {
+    .on('change', "input[type='file']", function () {
       if (!$(this)[0].files[0]) {
         return
-      }
-
-      let file = $(this)[0].files[0],
-        fileSize = (file.size / 1000000).toPrecision(2),
-        fileNameField = $(this)
-          .closest('.input_file')
-          .find('.input_file__filename'),
-        inputStructureCont = $(this).closest('.input-with-label'),
-        actualInputCont = $(this).closest('.input_file')
-
-      if (fileSize <= 20) {
-        fileNameField.text(file.name)
-        if (inputStructureCont.data().addInputFileOnEnd) {
-          const emptyInput = actualInputCont.clone()
-          clearInputTypeFile(emptyInput)
-          inputStructureCont.append(emptyInput)
-
-          actualInputCont.addClass('input_file-with-file')
-        }
       } else {
-        fileNameField.text($(this).data().fileIsTooBigMessage)
+        let file = $(this)[0].files[0],
+          fileSize = (file.size / 1000000).toPrecision(2),
+          fileNameField = $(this)
+            .closest('.input_file')
+            .find('.input_file__filename'),
+          inputStructureCont = $(this).closest('.input-with-label'),
+          actualInputCont = $(this).closest('.input_file')
+
+        if (fileSize <= 20) {
+          fileNameField.text(file.name)
+          if (inputStructureCont.data().addInputFileOnEnd) {
+            const emptyInput = actualInputCont.clone()
+            clearInputTypeFile(emptyInput)
+            inputStructureCont.append(emptyInput)
+
+            actualInputCont.addClass('input_file-with-file')
+          }
+        } else {
+          fileNameField.text($(this).data().fileIsTooBigMessage)
+        }
       }
     })
-    .on('click', '.input_file .input_file__btn_delete', function(e) {
-      $(this)
-        .closest('.input_file')
-        .remove()
+    .on('click', '.input_file .input_file__btn_delete', function (e) {
+      $(this).closest('.input_file').remove()
       e.preventDefault()
     })
 })
