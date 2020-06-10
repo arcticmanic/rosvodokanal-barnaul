989c565bd4143cb5a846ac5d5a4b34839d474d3c
Fromaline <grechino2412@gmail.com>
2020-06-10 11:36:52 +0300

    remove career-tpl.js code

--- js/common/career-tpl/career-tpl.js
+++ js/common/career-tpl/career-tpl.js
@@ -1,17 +1,17 @@
-$(document).ready(function () {
-  $(document)
-    .on('modal:before-open', '.modal_job', function (event, modal) {
-      const jobTitle = modal.$anchor
-        .first()
-        .closest('.page-content__title-cont')
-        .find('.page-content__title')
-        .text()
-      modal.$elm.first().find('.form-title').text(jobTitle)
-    })
-    .on('modal:after-close', '.modal_job', function (event, modal) {
-      clearInputTypeFile($('.modal_job .input_file'))
-      modal.$elm.first().find('.form-title').text('')
-      const allInputsInModal = modal.$elm.first().find('input[type="text"], textarea')
-      clearAllTextInputsAndTextareas(allInputsInModal)
-    })
-})
+// $(document).ready(function () {
+//   $(document)
+//     .on('modal:before-open', '.modal_job', function (event, modal) {
+//       const jobTitle = modal.$anchor
+//         .first()
+//         .closest('.page-content__title-cont')
+//         .find('.page-content__title')
+//         .text()
+//       modal.$elm.first().find('.form-title').text(jobTitle)
+//     })
+//     .on('modal:after-close', '.modal_job', function (event, modal) {
+//       clearInputTypeFile($('.modal_job .input_file'))
+//       modal.$elm.first().find('.form-title').text('')
+//       const allInputsInModal = modal.$elm.first().find('input[type="text"], textarea')
+//       clearAllTextInputsAndTextareas(allInputsInModal)
+//     })
+// })
